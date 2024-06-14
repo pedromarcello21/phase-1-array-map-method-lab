2b82477ab575f3c70c07ad12d518c13274f74554
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-06-14 17:04:53 -0400

    first commit

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () =>{
+  return tutorials.map(tutorial =>{
+    const words = tutorial.split(" ");
+    const capitalizedWords = words.map(word =>{
+      return word.charAt(0).toUpperCase() +word.slice(1)
+    })
+    return capitalizedWords.join(" ")
+  })
 }
+
+console.log((titleCased()))
+
